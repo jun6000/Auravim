1114c213a5d3044367cee3cb5d832143f9facdeb
Arjun Vishanth <arjunvishanth@gmail.com>
2023-09-08 13:00:50 +0530

    Added autosetup to packer.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,7 +1,17 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[ packadd packer.nvim ]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -83,9 +93,9 @@ return require('packer').startup(function(use)
 
     --[[ -- Tabnine
     use {
-        'tzachar/cmp-tabnine',
-        run = './install.sh',
-        requires = 'hrsh7th/nvim-cmp',
+    'tzachar/cmp-tabnine',
+    run = './install.sh',
+    requires = 'hrsh7th/nvim-cmp',
     } ]]
 
     -- Comment
@@ -119,4 +129,9 @@ return require('packer').startup(function(use)
         config = [[require('dash')]],
         requires = {'nvim-tree/nvim-web-devicons'}
     }
+
+    -- Auto setup config after cloning packer.nvim
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
